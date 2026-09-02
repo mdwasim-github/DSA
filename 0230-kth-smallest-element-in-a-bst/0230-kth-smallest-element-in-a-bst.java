2136dd5a954f0bb1859d55d5c0096d07549c5d9e
mdwasim-github <151392869+mdwasim-github@users.noreply.github.com>
2026-09-02 23:07:42 +0530

    Time: 1 ms (38.09%), Space: 46.5 MB (89.47%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ 
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        
+    }
+}
+*/
+
+
+class Solution {
+    
+    ArrayList<Integer> al = new ArrayList<>();
+    
+    void helper(TreeNode root) {
+        
+        if(root == null) return ;
+        
+        helper(root.left);
+        al.add(root.val);
+        helper(root.right);
+        
+    }
+    
+    
+    public int kthSmallest(TreeNode root, int k) {
+        
+        helper( root);
+        
+        return al.get(k-1);
+        
+        
+    }
+}
