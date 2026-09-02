31f2ad3b60d256adb82cfdb6771aad3c77e8abd9
mdwasim-github <151392869+mdwasim-github@users.noreply.github.com>
2026-09-02 23:40:12 +0530

    Time: 3 ms (99.8%), Space: 46.6 MB (25.63%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String intToRoman(int num) {
+        // Define values and symbols in descending order
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        StringBuilder roman = new StringBuilder();
+        
+        // Loop through the values greedily
+        for (int i = 0; i < values.length; i++) {
+            // While the current value can fit into num
+            while (num >= values[i]) {
+                roman.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        
+        return roman.toString();
+    }
+}
